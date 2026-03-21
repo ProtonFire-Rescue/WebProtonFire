470b3b87edae5211aa0193aef5246c51c4ba1a45
MaicolCode <paucarjhon07@outlook.com>
2026-03-20 20:21:35 -0500

    chore: Remove trailing path from site URL and update EmailSend import to use path alias

--- client/astro.config.mjs
+++ client/astro.config.mjs
@@ -11,7 +11,7 @@ import cloudflare from '@astrojs/cloudflare';
 export default defineConfig({
   output: 'server',
   adapter: cloudflare(),
-  site: 'https://webprotonfire.tecnologia-14a.workers.dev/_actions/send/',
+  site: 'https://webprotonfire.tecnologia-14a.workers.dev',
 
   vite: {
     plugins: [tailwindcss()]  
